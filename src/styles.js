8b9fc38fd4cfdaaca578e36d0610a288e391e0a1
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2025-04-24 16:19:18 +0100

    Service Card

--- src/styles.js
+++ src/styles.js
@@ -4,9 +4,9 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[70px] sm:text-[50px] xs:text-[40px] text-[30px] lg:leading-[88px] mt-2",
+    "font-black text-white lg:text-[70px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[88px] mt-2",
   heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[16px] xs:text-[10px] text-[6px] lg:leading-[20px]",
+    "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[16px] xs:text-[12px] text-[10px] lg:leading-[20px]",
 
   sectionHeadText:
     "text-white font-black md:text-[50px] sm:text-[40px] xs:text-[30px] text-[20px]",
